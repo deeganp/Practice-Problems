cfc1493c6035fbfacfbaddd66a811d5cf47c8b24
deeganp <deeganp.motro@gmail.com>
2024-03-04 20:29:41 -0500

    more

--- Python Problems/isPalindrome.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        clean_s = ''.join(ch.lower() for ch in s if ch.isalnum())
-        return clean_s == clean_s[::-1]
-        """
-        :type s: str
-        :rtype: bool
-        """
