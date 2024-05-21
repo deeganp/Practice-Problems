137ae0b6a25bf6cc7f36533b776d15b180baa74c
deeganp <deeganp.motro@gmail.com>
2024-05-21 19:54:01 -0400

    more

---
+++ Python Problems/sieve.py
@@ -0,0 +1,15 @@
+def sieve_of_eratosthenes(n):
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+
+    return [i for i in range(n + 1) if primes[i]]
+
+# Example usage:
+n = 100
+prime_numbers = sieve_of_eratosthenes(n)
+print("Prime numbers up to", n, "are:", prime_numbers)
