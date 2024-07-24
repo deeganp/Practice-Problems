f434fd9c15b296a931b1040af63bfba8455a5434
deeganp <deeganp.motro@gmail.com>
2024-07-24 17:18:36 -0400

    Update symmetricTree.py

--- Python Problems/symmetricTree.py
+++ Python Problems/symmetricTree.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -5,16 +8,15 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def isSymmetric(self, root: TreeNode) -> bool:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
-        return self.isMirror(root.left, root.right)
-    
-    def isMirror(self, left: TreeNode, right: TreeNode) -> bool:
-        if not left and not right:
-            return True
-        if not left or not right:
-            return False
-        return (left.val == right.val and 
-                self.isMirror(left.left, right.right) and 
-                self.isMirror(left.right, right.left))
+        
+        def is_mirror(t1: Optional[TreeNode], t2: Optional[TreeNode]) -> bool:
+            if not t1 and not t2:
+                return True
+            if not t1 or not t2:
+                return False
+            return (t1.val == t2.val) and is_mirror(t1.right, t2.left) and is_mirror(t1.left, t2.right)
+        
+        return is_mirror(root.left, root.right) 
