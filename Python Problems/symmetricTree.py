75e7a61a60510fd7a2dde9809f8cc313732e7d94
deeganp <deeganp.motro@gmail.com>
2024-07-24 16:55:14 -0400

    Create symmetricTree.py

---
+++ Python Problems/symmetricTree.py
@@ -0,0 +1,20 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
+    
+    def isMirror(self, left: TreeNode, right: TreeNode) -> bool:
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return (left.val == right.val and 
+                self.isMirror(left.left, right.right) and 
+                self.isMirror(left.right, right.left))
